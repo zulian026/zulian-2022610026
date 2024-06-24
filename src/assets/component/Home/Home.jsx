1ad82f6a6cfc7a1cf8397fa32e34824a279e2df1
zulian <zulianalhisyam@gmail.com>
2024-06-24 18:42:23 +0700

    tiga change

--- src/assets/component/Home/Home.jsx
+++ src/assets/component/Home/Home.jsx
@@ -1,27 +1,64 @@
 import React, { useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 import './Home.css';
-import Aos from 'aos';
-import 'aos/dist/aos.css'
-import homeImage from '../image/home_image.jpg'; // Assuming the image is in the same directory and named 'home_image.jpg'
+import homeImage from '../image/home_image.jpg';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Home = () => {
   useEffect(() => {
-    Aos.init();
-  }, [])
+    AOS.init();
+  }, []);
+
   return (
     <div className="home-kontainer">
       <div className="boxx-1">
-        <div className="content-satu" data-aos="fade-right" data-aos-duration="2000">
+        <div className="content-satu" data-aos="fade-right"  data-aos-duration="3000"> 
           <h1><span>Ngoding Bareng</span><br /> di Website Saya</h1>
           <p>Ini adalah halaman utama dari website yang menyediakan tutorial ngoding untuk pemula.</p>
         </div>
-
-        <div className="content-dua" data-aos="fade-left" data-aos-duration="2000">
+        <div className="content-dua">
           <img src={homeImage} alt="Coding Illustration" />
         </div>
 
         <div className="content-tiga">
           <h2>List tutorial</h2>
+          <div className="card-container">
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Belajar Ngoding #1</Card.Title>
+                <Card.Img src={homeImage} style={{ width: '100%', height: 'auto' }} />
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Belajar Ngoding #2</Card.Title>
+                <Card.Img src={homeImage} style={{ width: '100%', height: 'auto' }} />
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }}>
+              <Card.Body>
+                <Card.Title>Belajar Ngoding #3</Card.Title>
+                <Card.Img src={homeImage} style={{ width: '100%', height: 'auto' }} />
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+          <hr />
         </div>
       </div>
     </div>
